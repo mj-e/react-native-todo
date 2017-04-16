b39081ad781f4b4ce2662f12b598a05dccb68afe
mj-e <matt_evans30@yahoo.com>
2017-04-16 10:54:46 +0100

    Initial commit todo list working with 3 footer buttons

---
+++ app.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform, TextInput, ListView, Keyboard } from 'react-native';
+import Header from './header';
+import Footer from './footer';
+import Row from './row';
+
+const filterItems = (filter, items) => {
+    return items.filter((item) => {
+        if (filter === 'ALL') return true;
+        if (filter === 'COMPLETED') return item.complete;
+        if (filter === 'ACTIVE') return !item.complete;
+    })
+}
+class App extends Component {
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+        this.state = {
+            allComplete: false,
+            value: '',
+            items: [],
+            dataSource: ds.cloneWithRows([]),
+            filter: 'ALL'
+        }
+        this.handleFilter = this.handleFilter.bind(this);
+        this.handleRemoveItem = this.handleRemoveItem.bind(this);
+        this.handleToggleComplete = this.handleToggleComplete.bind(this);
+        this.handleAddItem = this.handleAddItem.bind(this);
+        this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+        this.setSource = this.setSource.bind(this);
+
+
+    }
+    setSource(items, itemsDatasource, otherState = {}) {
+        this.setState({
+            items,
+            dataSource: this.state.dataSource.cloneWithRows(itemsDatasource),
+            ...otherState
+        })
+    }
+    handleFilter(filter) {
+        this.setSource(this.state.items, filterItems(filter, this.state.items, { filter }))
+    }
+    handleRemoveItem(key) {
+        const newItems = this.state.items.filter((item) => {
+            return item.key !== key
+        })
+        this.setSource(newItems, filterItems(this.state.filter, newItems));
+    }
+    handleToggleComplete(key, complete) {
+        const newItems = this.state.items.map((item) => {
+            if (item.key !== key) return item;
+            return {
+                ...item,
+                complete
+            }
+        })
+        this.setSource(newItems, filterItems(this.state.filter, newItems))
+
+    }
+    handleToggleAllComplete() {
+        const complete = !this.state.allComplete;
+        const newItems = this.state.items.map((item) => ({
+            ...item,
+            complete
+        }))
+        this.setSource(newItems, filterItems(this.state.filter, newItems), { allComplete: complete })
+    }
+    handleAddItem() {
+        if (!this.state.value) return;
+        const newItems = [
+            ...this.state.items,
+            {
+                key: Date.now(),
+                text: this.state.value,
+                complete: false
+            }
+        ]
+        this.setSource(newItems, filterItems(this.state.filter, newItems), { value: '' })
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header
+                    value={this.state.value}
+                    onAddItem={this.handleAddItem}
+                    onChange={(value) => this.setState({ value })}
+                    onToggleAllComplete={this.handleToggleAllComplete}
+                />
+                <View style={styles.content}>
+                    <ListView
+                        style={styles.list}
+                        enableEmptySections
+                        dataSource={this.state.dataSource}
+                        onScroll={() => Keyboard.dismiss()}
+                        renderRow={({ key, ...value }) => {
+                            return (
+                                <Row
+                                    key={key}
+                                    onRemove={() => this.handleRemoveItem(key)}
+                                    onComplete={(complete) => this.handleToggleComplete(key, complete)}
+                                    {...value}
+                                />
+                            )
+                        }}
+                        renderSeperator={(sectionId, rowId) => {
+                            return <View key={rowId} style={styles.seperator} />
+                        }}
+                    />
+
+                </View>
+                <Footer
+                    onFilter={this.handleFilter}
+                    filter={this.state.filter}
+
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#A5A5A5',
+        ...Platform.select({
+            ios: { paddingTop: 30 }
+        })
+    },
+    content: {
+        flex: 1
+    },
+    list: {
+        backgroundColor: '#FFF'
+    },
+    separator: {
+        borderWidth: 1,
+        borderColor: '#F5F5F5'
+    }
+})
+
+
+
+export default App;
---
+++ row.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Switch, TouchableOpacity } from 'react-native';
+
+class Row extends Component {
+    render() {
+        const {complete} = this.props;
+        return (
+            <View style={styles.container}>
+                <Switch
+                    value={complete}
+                    onValueChange={this.props.onComplete}
+                />
+                <View style={styles.textWrap}>
+                    <Text style={[styles.text, complete && styles.complete]}>{this.props.text}</Text>
+                </View>
+                <TouchableOpacity onPress={this.props.onRemove}>
+                    <Text style={styles.destroy}>X</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between'
+    },
+    textWrap: {
+        flex: 1,
+        marginHorizontal: 10
+    },
+    complete: {
+        textDecorationLine: 'line-through'
+    },
+    text: {
+        fontSize: 24,
+        color: '#4d4d4d'
+    },
+    destroy: {
+        fontSize: 20,
+        color: '#cc9a9a'
+    }
+})
+
+export default Row;
